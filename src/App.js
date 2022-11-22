bea0ff82a8b67e43f6dedeb3076e7e56c3b67ccd
chatmongcol <cdadwngs@gmail.com>
2022-11-22 17:34:07 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import './App.css'
+import Profile from './components/pages/Profile'
+import Contact from './components/pages/Contact'
+import About from './components/pages/About'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App-header">
+        <h1>Welcome to React</h1>
+        <hr />
+        <About />
+        <Profile />
+        <Contact />
+      </div>
+    </>
+  )
 }
-
-export default App;
+export default App
